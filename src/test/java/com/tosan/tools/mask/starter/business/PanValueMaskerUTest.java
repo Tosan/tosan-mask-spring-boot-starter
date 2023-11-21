d6f29c7008e9523a89451452d52354b8c9775b62
mosidev <abdollahi.dev@gmail.com>
2023-11-21 19:55:07 +0330

    [revision] edit pan mask unit tests

--- src/test/java/com/tosan/tools/mask/starter/business/PanValueMaskerUTest.java
+++ src/test/java/com/tosan/tools/mask/starter/business/PanValueMaskerUTest.java
@@ -1,56 +1,57 @@
 package com.tosan.tools.mask.starter.business;
 
 import com.tosan.tools.mask.starter.business.enumeration.MaskType;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 /**
  * @author M.khoshnevisan
  * @since 7/3/2021
  */
 public class PanValueMaskerUTest {
-
-    private PanValueMasker panValueMask = new PanValueMasker();
+    private final PanValueMasker panValueMask = new PanValueMasker();
 
     @Test
     public void testGetType_returnPan() {
         MaskType type = panValueMask.getType();
-        Assertions.assertEquals(type, MaskType.PAN);
+        assertEquals(type, MaskType.PAN);
     }
 
     @Test
-    public void testPantMask_nullParameter_returnEmptyString() {
+    public void testPantMask_nullParameter_returnNull() {
         String maskedValue = panValueMask.mask(null);
-        Assertions.assertEquals(maskedValue, "");
+        assertNull(maskedValue);
     }
 
     @Test
     public void testPanMask_emptyParameter_returnEmptyString() {
         String maskedValue = panValueMask.mask("");
-        Assertions.assertEquals(maskedValue, "");
+        assertEquals(maskedValue, "");
     }
 
     @Test
     public void testPanMask_parameterLessThanTwoCharacters_returnCorrectMaskedString() {
         String maskedValue = panValueMask.mask("5");
-        Assertions.assertEquals(maskedValue, "*ENCRYPTED");
+        assertEquals(maskedValue, "*ENCRYPTED");
     }
 
     @Test
     public void testPanMask_with16CharacterParameter_returnCorrectMaskedString() {
         String maskedValue = panValueMask.mask("5876546533454650");
-        Assertions.assertEquals(maskedValue, "*SEMI_ENCRYPTED:587654******4650");
+        assertEquals(maskedValue, "587654******4650");
     }
 
     @Test
     public void testPanMask_with19CharacterParameter_returnCorrectMaskedString() {
         String maskedValue = panValueMask.mask("5876546533454650876");
-        Assertions.assertEquals(maskedValue, "*SEMI_ENCRYPTED:587654*********0876");
+        assertEquals(maskedValue, "587654*********0876");
     }
 
     @Test
     public void testPanMask_withLessThan16CharacterParameter_returnCorrectMaskedString() {
         String maskedValue = panValueMask.mask("587654653345465");
-        Assertions.assertEquals(maskedValue, "*SEMI_ENCRYPTED:5876546");
+        assertEquals(maskedValue, "58765*****45465");
     }
 }
